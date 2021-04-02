99b24e4e7787d3e9ef3735234d3c589038e106de
MResweber <mres@MatthewLaptop.localdomain>
2021-04-02 17:53:16 -0400

    Cleaned code

--- ShellProject/myCat.c
+++ ShellProject/myCat.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * prints any number of files concatenated together to stdout
+ */
 int main (int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
-        char *path = argv[i];
+        char *path = argv[i];               // The path to the current file being read
         FILE *f = fopen(path, "r");
 
-        char ch;
+        char ch;                            // The current character being read
         while ((ch = fgetc(f)) != EOF) {
             printf("%c", ch);
         }
-        printf("%c", EOF);
     }
 }
